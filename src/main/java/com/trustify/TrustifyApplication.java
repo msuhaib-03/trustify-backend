72c426e8307f73fa2b8e2f693a917d213b351e21
adrenalinejunkie03 <muhammadsuhaib2805@gmail.com>
2025-10-29 21:19:27 +0500

    Merged Springboot project with existing repo

---
+++ src/main/java/com/trustify/TrustifyApplication.java
@@ -0,0 +1,13 @@
+package com.trustify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrustifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrustifyApplication.class, args);
+	}
+
+}
