4d265674c23e0254710cc64ab8e8ee33a6e7245d
adrenalinejunkie03 <muhammadsuhaib2805@gmail.com>
2026-01-07 15:07:15 +0500

    change on springconfig

--- src/main/java/com/trustify/config/SecurityConfig.java
+++ src/main/java/com/trustify/config/SecurityConfig.java
@@ -36,7 +36,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                //.cors(Customizer.withDefaults())
+                .cors(Customizer.withDefaults())
                 .cors(cors -> cors.configurationSource(corsConfig.corsConfigurationSource()))
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
