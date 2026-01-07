7d9a514c61ac33f3f0539a631bc1fee3aede8669
adrenalinejunkie03 <muhammadsuhaib2805@gmail.com>
2026-01-07 15:09:11 +0500

    change on springconfig & corsconfig

--- src/main/java/com/trustify/config/CorsConfig.java
+++ src/main/java/com/trustify/config/CorsConfig.java
@@ -13,64 +13,64 @@ import java.util.List;
 @Configuration
 public class CorsConfig {
     // CORS configuration can be added here if needed in the future
-    @Bean
-    public CorsFilter corsFilter() {
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:3001","http://localhost:5173", "http://localhost:5174"));
-        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
-       // config.setAllowedHeaders(Arrays.asList("*"));
-        config.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
-        config.setAllowCredentials(true);
-        config.setMaxAge(3600L);
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", config);
-
-        return new CorsFilter(source);
-    }
-
-
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration config = new CorsConfiguration();
-
-        config.addAllowedOrigin("http://localhost:5174");
-        config.setAllowCredentials(true);
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("*");
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", config);
-
-        return source;
-    }
+//    @Bean
+//    public CorsFilter corsFilter() {
+//        CorsConfiguration config = new CorsConfiguration();
+//        config.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:3001","http://localhost:5173", "http://localhost:5174"));
+//        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
+//       // config.setAllowedHeaders(Arrays.asList("*"));
+//        config.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+//        config.setAllowCredentials(true);
+//        config.setMaxAge(3600L);
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", config);
+//
+//        return new CorsFilter(source);
+//    }
+//
+//
+//    @Bean
+//    public CorsConfigurationSource corsConfigurationSource() {
+//        CorsConfiguration config = new CorsConfiguration();
+//
+//        config.addAllowedOrigin("http://localhost:5174");
+//        config.setAllowCredentials(true);
+//        config.addAllowedHeader("*");
+//        config.addAllowedMethod("*");
+//
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", config);
+//
+//        return source;
+//    }
 
 
 
 
     // MY PREVIOUS CODE
-//    @Bean
-//    public CorsConfigurationSource corsConfigurationSource() {
-//        CorsConfiguration corsConfig = new CorsConfiguration();
-//        corsConfig.setAllowedOrigins(List.of(
-//                "http://localhost:3000",
-//                "http://localhost:5173",
-//                "http://localhost:5174",
-//                "http://52.66.8.174",
-//                "http://localhost:8080",
-//                "http://localhost:3001"
-//
-//        ));
-//
-//        corsConfig.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-//        //corsConfig.setAllowedHeaders(List.of("*"));
-//        corsConfig.setAllowCredentials(true);
-//        //corsConfig.setExposedHeaders(List.of("Content-Type","Authorization"));
-//        corsConfig.setAllowedHeaders(List.of("Content-Type","Authorization"));
-//        corsConfig.setExposedHeaders(List.of("Authorization"));
-//
-//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//        source.registerCorsConfiguration("/**", corsConfig);
-//        return source;
-//
-//    }
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.setAllowedOrigins(List.of(
+                "http://localhost:3000",
+                "http://localhost:5173",
+                "http://localhost:5174",
+                "http://52.66.8.174",
+                "http://localhost:8080",
+                "http://localhost:3001"
+
+        ));
+
+        corsConfig.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        //corsConfig.setAllowedHeaders(List.of("*"));
+        corsConfig.setAllowCredentials(true);
+        //corsConfig.setExposedHeaders(List.of("Content-Type","Authorization"));
+        corsConfig.setAllowedHeaders(List.of("Content-Type","Authorization"));
+        corsConfig.setExposedHeaders(List.of("Authorization"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+        return source;
+
+    }
 }
