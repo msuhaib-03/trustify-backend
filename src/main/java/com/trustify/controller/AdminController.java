d779567c9e52fc9c94c1b90b3d4ce52caf54240a
adrenalinejunkie03 <muhammadsuhaib2805@gmail.com>
2026-05-19 13:56:05 +0500

    from hasRole to hasAuthority in AdminController

--- src/main/java/com/trustify/controller/AdminController.java
+++ src/main/java/com/trustify/controller/AdminController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/admin")
-@PreAuthorize("hasRole('ADMIN')")
+@PreAuthorize("hasAuthority('ADMIN')")
 public class AdminController {
 
     @Autowired
