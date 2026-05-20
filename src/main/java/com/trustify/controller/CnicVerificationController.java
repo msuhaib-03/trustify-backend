0c7c0a4871d40cbc831371549163cb9308c049a1
adrenalinejunkie03 <muhammadsuhaib2805@gmail.com>
2026-05-20 06:48:59 +0500

    fetching Id instead of email from mongo in CNIC Controller

--- src/main/java/com/trustify/controller/CnicVerificationController.java
+++ src/main/java/com/trustify/controller/CnicVerificationController.java
@@ -1,5 +1,7 @@
 package com.trustify.controller;
 
+import com.trustify.model.User;
+import com.trustify.repository.UserRepository;
 import com.trustify.service.CnicVerificationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -14,8 +16,10 @@ import java.security.Principal;
 public class CnicVerificationController {
     @Autowired
     CnicVerificationService cnicVerificationService;
+    @Autowired
+    private UserRepository userRepository;
 
-     // This controller will handle endpoints related to CNIC verification, such as submitting CNIC details and fetching verification status.
+    // This controller will handle endpoints related to CNIC verification, such as submitting CNIC details and fetching verification status.
 
     @PostMapping("/submit")
     @PreAuthorize("hasAuthority('USER')")
@@ -25,9 +29,17 @@ public class CnicVerificationController {
             Principal principal
     ) {
 
+        // logic changed here because we want actual userId and not email.
+        // We have been fetching user email instead of actual mongoDB userId in
+        // all the services and controllers. So we will change that to userId now.
+        // And for that we will need to fetch user details using principal.getName() which is email and then get userId from there and pass it to service layer.
+        User user = userRepository.findByEmail(principal.getName())
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
         return ResponseEntity.ok(
                 cnicVerificationService.submitVerifiction(
-                        principal.getName(),
+                      //  principal.getName(), // this was fetching email instead of userId
+                        user.getId(), // this fetches actual userId from database using email from principal
                         frontImage,
                         backImage
                 )
